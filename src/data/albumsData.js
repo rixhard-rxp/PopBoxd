69c3904382cc5ead338ff46ef042fafa3e5b0a58
rixhard-rxp <oskitarmolpa@gmail.com>
2026-06-29 18:36:38 -0500

    feat: Creación del entorno de trabajo, más archivo con los datos necesarios para la información de los albums

---
+++ src/data/albumsData.js
@@ -0,0 +1,29 @@
+export const ALBUMS_DATA = [
+  {
+    id: '1',
+    title: 'BRAT',
+    artist: 'Charli XCX',
+    year: '2024',
+    cover: require('../../assets/brat.jpg'), // Ruta relativa hacia tu carpeta assets
+    color: '#8ACE00',
+    tracks: ['360', 'Club classics', 'Sympathy is a knife', 'Von dutch', 'Everything is romantic']
+  },
+  {
+    id: '2',
+    title: 'MOTOMAMI',
+    artist: 'Rosalía',
+    year: '2022',
+    cover: require('../../assets/motomami.jpg'),
+    color: '#FF0000',
+    tracks: ['SAOKO', 'CANDY', 'LA FAMA', 'BULERÍAS', 'CHICKEN TERIYAKI']
+  },
+  {
+    id: '3',
+    title: 'Heaven Knows',
+    artist: 'PinkPantheress',
+    year: '2023',
+    cover: require('../../assets/heaven.jpg'),
+    color: '#E6C5E3',
+    tracks: ['Another life', 'True romance', 'Mosquito', 'The layton', 'Caprice']
+  }
+];
